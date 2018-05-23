154c4d4a3b06d371ec74c6bee7fe889fea15836f
anilreddyg <anil.reddy@wooga.com>
2018-05-23 18:54:30 +0200

    setup to receive version requests at /check_package_version

--- venv/wta_server_request_router.py
+++ venv/wta_server_request_router.py
@@ -4,32 +4,36 @@ from flask import request
 from flask import  render_template
 from pymongo import MongoClient
 import datetime
+from distutils.version import LooseVersion
 app = Flask(__name__)
 
 
 # preparing db objects
 client = MongoClient()
 db = client.wta_tools
+# collections
 tool_events = db.tool_events
-tools = db.tools
-users = db.users
+db_tools = db.tools
+db_users = db.users
+db_packages = db.packages
 
 
 @app.route('/reports')
 def render_reports():
+	print("\nreceived report request at {}\n".format(datetime.datetime.now()))
 	# EXPORT TABLE
 	export_report = list()
-	for user in users.find():
+	for user in db_users.find():
 		export_report.append({'user_name': user['user_name']})
 	# print(export_table)
 
 	# for each user, get the export count
-	exporter_tool_id = tools.find_one({"tool_name": "exporter"})['_id']
+	exporter_tool_id = db_tools.find_one({"tool_name": "exporter"})['_id']
 	total_exported_items = 0
 	for i, export_report_row in enumerate(export_report):
 		export_calls = 0
 		exported_items = 0
-		user_id = users.find_one({"user_name":export_report_row['user_name']})['_id']
+		user_id = db_users.find_one({"user_name":export_report_row['user_name']})['_id']
 		# find all export events related to the current user
 		for export_event in tool_events.find({"tool_id": ObjectId(exporter_tool_id), "user_id": ObjectId(user_id)}):
 			export_calls += 1
@@ -40,13 +44,13 @@ def render_reports():
 
 	# TOOLS REPORT
 	tools_report = list()
-	for tool in tools.find():
+	for tool in db_tools.find():
 		tools_report.append({ 'tool_name': tool['tool_name']})
 	# print(tools_report)
 
 	# for each tool in the tool report, check the tool events for usage
 	for i, tools_report_row in enumerate(tools_report):
-		tool_id = tools.find_one({"tool_name": tools_report_row['tool_name']})['_id']
+		tool_id = db_tools.find_one({"tool_name": tools_report_row['tool_name']})['_id']
 		tool_usage_count = 0
 		for tool_event in tool_events.find({"tool_id": ObjectId(tool_id)}):
 			tool_usage_count += 1
@@ -60,16 +64,16 @@ def render_reports():
 def update_usage_in_db(*args):
 	print("\nRegistered tool usage!\n")
 	# check if the user exists, if not add to users
-	user = users.find_one({"user_name": request.args['user_name']})
+	user = db_users.find_one({"user_name": request.args['user_name']})
 	if user is None:
-		user_id = users.insert({"user_name": request.args['user_name']})
+		user_id = db_users.insert({"user_name": request.args['user_name']})
 	else:
 		user_id = user['_id']
 
 	# check if tool exists, if not, add
-	tool = tools.find_one({"tool_name": request.args['tool_name']})
+	tool = db_tools.find_one({"tool_name": request.args['tool_name']})
 	if tool is None:
-		tool_id = tools.insert({"tool_name": request.args['tool_name']})
+		tool_id = db_tools.insert({"tool_name": request.args['tool_name']})
 	else:
 		tool_id = tool['_id']
 
@@ -87,3 +91,14 @@ def update_usage_in_db(*args):
 	# insert event dict into events collection in db
 	tool_events.insert_one(event_dict)
 	return "here have a return"
+
+
+# http://localhost:5000/check_package_version?package_id=com.wta-photoshop&current_version=0.0.1
+@app.route('/check_package_version', methods=['GET'])
+def check_package_version(*args):
+	print("\nreceived version request at {}\n".format(datetime.datetime.now()))
+	latest_version = db_packages.find_one({"package_id":request.args['package_id']})['latest_version']
+	current_version = request.args['current_version']
+	if LooseVersion(latest_version) > LooseVersion(current_version):
+		return "outdated"
+	return "up to date"
